75a8fcefd5ad796a59d6b121403643f892912087
Antonio De Rosa <antonio.derosa@akhela.com>
2014-07-17 10:39:28 +0200

    GUI changed and other important issues

--- gui.py
+++ gui.py
@@ -23,19 +23,19 @@ class GUI(object):
     def __init__(self):
         global builder
         builder = Gtk.Builder()
-        builder.add_from_file("gui.glade")
-        builder.connect_signals(self)    
+        builder.add_from_file("gui2.glade")
+        builder.connect_signals(self)
 
+        self.ni = NetworkInterfaces()
         self.update_netiface_info(builder.get_object("combobox_ifs"))           
         self.label_append (builder.get_object("lbl_hostname"), "\"{}\"".format(gethostname()))
 
     def update_netiface_info(self, combobox):
-        self.ni = NetworkInterfaces()
-        #combobox.removeall()
-        map (combobox.append_text, filter (lambda iface: iface != self.ni.get_default(), self.ni.get_interfaces()))
+        self.ni.update()
+        combobox.remove_all()
         combobox.append_text(self.ni.get_default() + " (default)")
-        combobox.set_active(len(self.ni.get_interfaces()) - 1)
-        LOG.debug(self.ni.get_default())
+        map (combobox.append_text, filter (lambda iface: iface != self.ni.get_default(), self.ni.get_interfaces()))        
+        combobox.set_active(0)        
 
     def write_to_buffer(self, fd, condition):
         if condition == glib.IO_IN:     #if there's something interesting to read
@@ -55,31 +55,31 @@ class GUI(object):
 
     def on_switch_pressed(self, switch, gparam):           
         map (lambda c: c.set_sensitive(switch.get_active()), builder.get_object("grid2").get_children())  
-
-        #proc = subprocess.Popen(['/bin/sh', '-c', './test.sh'], stdout = subprocess.PIPE) 
         proc = subprocess.Popen(['gksudo', 'sh', './anonymous', 'status'], stdout = subprocess.PIPE) 
         glib.io_add_watch(proc.stdout, glib.IO_IN,  self.write_to_buffer)           
         #MyTask().execute()
 
     def on_combobox_ifs_changed(self, combobox):
-        ifname_active = builder.get_object("combobox_ifs").get_active_text()
-        builder.get_object("lbl_mac").set_text("The current {} MAC address is {}".format(ifname_active, self.ni.get_addr(ifname_active)))
+        if combobox.get_active_text() is None:
+            return
 
-    def on_label_click(self, checkbutton, gparam):
-        checkbutton.set_active(not checkbutton.get_active())
+        ifname = self.ni.get_default() if "(default)" in combobox.get_active_text() else combobox.get_active_text()
+        builder.get_object("lbl_mac").set_text("Your current MAC address " + ("is the real-one" if self.ni.is_spoofed(ifname) else "is already spoofed!"))        
+        builder.get_object("lbl_mac").set_visible(not self.ni.is_spoofed(ifname))
+        builder.get_object("checkbutton1").set_active(not self.ni.is_spoofed(ifname))
 
     def toggle_show(self, togglecontent):
         togglecontent.set_visible(not togglecontent.get_visible())
 
     def toggle_show_tor(self, togglecontent):
         if not togglecontent.get_visible():
-            togglecontent.set_text(self.check_tor())   
+            togglecontent.set_text("You " + ("are"  if self.check_tor() else "are not") + " using Tor")   
 
         self.toggle_show(togglecontent)
 
     def check_tor(self):
         html = urllib2.urlopen('https://check.torproject.org/?lang=en_US').read()
-        return "You " + ("are"  if "Congratulations. This browser is" in html else "are not") + " using Tor"        
+        return True  if "Congratulations. This browser is" in html else False
 
     def label_append(self, label, text):
         label.set_text(label.get_text() + text)        
--- util.py
+++ util.py
@@ -1,11 +1,13 @@
 
-import netifaces
+import netifaces, re, subprocess
 
 class NetworkInterfaces:
 	def __init__(self):
+		self.pattern = re.compile(r"([0-9A-F]{2}[:-]){5}([0-9A-F]{2})", re.I)
 		self.interfaces = netifaces.interfaces()
 		self.ifaddresses = {iface:netifaces.ifaddresses(iface)[netifaces.AF_LINK][0]['addr'] for iface in self.interfaces}
 		self.default_gw = netifaces.gateways()['default'][netifaces.AF_INET][1]
+		self.ifspoofed = {iface:self.check_current_mac_address(iface) for iface in self.interfaces}
 
 	def get_interfaces(self):
 		return self.interfaces
@@ -14,11 +16,21 @@ class NetworkInterfaces:
 		return self.default_gw
 	
 	def get_addr(self, ifname):
-		if ifname not in self.interfaces:
-			return None
+		return self.ifaddresses[ifname] if ifname in self.ifaddresses.keys() else None
 
-		return self.ifaddresses[ifname]
+	def get_number(self):
+		return len(self.interfaces)
 
+	def update(self):
+		self.__init__()
+
+	def is_spoofed(self, ifname):
+		return self.ifspoofed[ifname] if ifname in self.ifspoofed.keys() else None
+
+	def check_current_mac_address(self, iface):		
+		proc = subprocess.Popen(['macchanger', '-s', iface], stdout=subprocess.PIPE)
+		macs = map (lambda line: self.pattern.search(line).group(), proc.stdout)
+		return macs[1:] == macs[:-1]
 
 class MyTask():
     def execute(self):

1745614e5dcf70e24b074993eb247679ea2ecdcf
Antonio De Rosa <antonio.derosa@akhela.com>
2014-07-17 17:36:21 +0200

    Other GUI changes

--- gui.py
+++ gui.py
@@ -27,8 +27,10 @@ class GUI(object):
         builder.connect_signals(self)
 
         self.ni = NetworkInterfaces()
+        self.wconsole = lambda text: builder.get_object("textview1").get_buffer().insert_at_cursor(text + '\n')
         self.update_netiface_info(builder.get_object("combobox_ifs"))           
-        self.label_append (builder.get_object("lbl_hostname"), "\"{}\"".format(gethostname()))
+        #self.label_append (builder.get_object("lbl_hostname"), "\"{}\"".format(gethostname()))
+        
 
     def update_netiface_info(self, combobox):
         self.ni.update()
@@ -54,7 +56,7 @@ class GUI(object):
         Gtk.main_quit(*args)
 
     def on_switch_pressed(self, switch, gparam):           
-        map (lambda c: c.set_sensitive(switch.get_active()), builder.get_object("grid2").get_children())  
+        map (lambda c: c.set_sensitive(switch.get_active()), builder.get_object("box").get_children())  
         proc = subprocess.Popen(['gksudo', 'sh', './anonymous', 'status'], stdout = subprocess.PIPE) 
         glib.io_add_watch(proc.stdout, glib.IO_IN,  self.write_to_buffer)           
         #MyTask().execute()
@@ -64,25 +66,38 @@ class GUI(object):
             return
 
         ifname = self.ni.get_default() if "(default)" in combobox.get_active_text() else combobox.get_active_text()
-        builder.get_object("lbl_mac").set_text("Your current MAC address " + ("is the real-one" if self.ni.is_spoofed(ifname) else "is already spoofed!"))        
-        builder.get_object("lbl_mac").set_visible(not self.ni.is_spoofed(ifname))
+        #builder.get_object("lbl_mac").set_text("Your current MAC address " + ("is the real-one" if self.ni.is_spoofed(ifname) else "is already spoofed!"))        
+        #builder.get_object("lbl_mac").set_visible(not self.ni.is_spoofed(ifname))
+        self.wconsole("{} current MAC address is {} ".format(ifname, "the real-one" if self.ni.is_spoofed(ifname) else "already spoofed!"))
         builder.get_object("checkbutton1").set_active(not self.ni.is_spoofed(ifname))
 
     def toggle_show(self, togglecontent):
         togglecontent.set_visible(not togglecontent.get_visible())
 
     def toggle_show_tor(self, togglecontent):
-        if not togglecontent.get_visible():
-            togglecontent.set_text("You " + ("are"  if self.check_tor() else "are not") + " using Tor")   
+        #if not togglecontent.get_visible():
+        self.wconsole("You " + ("are"  if self.check_tor() else "are not") + " using Tor") 
 
-        self.toggle_show(togglecontent)
+        #self.toggle_show(togglecontent)    
 
     def check_tor(self):
         html = urllib2.urlopen('https://check.torproject.org/?lang=en_US').read()
         return True  if "Congratulations. This browser is" in html else False
 
-    def label_append(self, label, text):
-        label.set_text(label.get_text() + text)        
+    def update_hostname_info(self, button):
+        if not self.check_hostname():
+            self.wconsole("The current hostname ({}) is already spoofed from the last boot!".format(gethostname()))
+            button.set_active(True)
+
+    def check_hostname(self):      
+        proc = subprocess.Popen(['fgrep', 'Linux version', '/var/log/kern.log'], stdout=subprocess.PIPE)
+        try:
+            hostname_original = proc.stdout.readline()[:-1].split()[3]
+        except:
+            LOG.debug("Unable to open kern.log or parsing it")
+            return True
+
+        return gethostname() == hostname_original
 
 if __name__ == '__main__':
     GUI().run()

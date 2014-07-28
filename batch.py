329f7bad83909c794ab8608990762ed7d2f09737
Antonio De Rosa <antonio.derosa@akhela.com>
2014-07-28 18:08:40 +0200

    Making robust Batch class and removing old files

--- batch.py
+++ batch.py
@@ -1,7 +1,8 @@
-import subprocess, shlex, glib, os
+import subprocess, shlex, glib, os, sys
 
 class Batch():
-	def __init__ (self, cmd = None, writer = None, callback = None):								
+	def __init__ (self, cmd = None, writer = None, callback = None):		
+		#self.log = lambda text: LOG.error(text)						
 		self.cmd = cmd
 		self.writer = writer
 		self.callback = callback
@@ -19,9 +20,18 @@ class Batch():
 		self.writer = writer
 
 	def run (self):
-		proc = subprocess.Popen (self.cmd, stdout = subprocess.PIPE)
-		glib.io_add_watch (proc.stdout, glib.IO_IN, self.writer)	
-		glib.child_watch_add (proc.pid, self.callback)
+		try:
+			proc = subprocess.Popen (self.cmd, stdout = subprocess.PIPE)
+		except OSError:
+			print "System-related error: '{}' does not seem to be a functional thing".format(self.cmd)
+		except:
+			print "Unexpected error:", sys.exc_info()[0]
+		else:
+			glib.io_add_watch (proc.stdout, glib.IO_IN, self.writer)	
+			glib.child_watch_add (proc.pid, self.callback)
+			return True
+
+		return False
 
 	def run_and_wait(self):
 		proc = subprocess.Popen (self.cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
